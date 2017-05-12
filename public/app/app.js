2f96c28127063a90272358fee5a1ca6a727f18f2
Nicholas Mooney <nicholas.a.mooney@gmail.com>
2017-05-12 16:44:06 -0600

    refactored some server side authorization

--- public/app/app.js
+++ public/app/app.js
@@ -1,6 +1,16 @@
 var app = angular.module('app', ['ngResource', 'ngRoute']);
 
-app.config(function($routeProvider, $locationProvider) {
+app.config(function ($routeProvider, $locationProvider) {
+    
+    var routeRoleChecks = {
+        admin: {
+            auth: function (mvAuth) {
+                return mvAuth.authorizeCurrentUserForRoute('admin');
+            }
+        }
+    };
+
+
     $locationProvider.html5Mode(true);
     $routeProvider
         .when('/', {
@@ -10,24 +20,15 @@ app.config(function($routeProvider, $locationProvider) {
         .when('/admin/users', {
             templateUrl: "/partials/admin/user-list",
             controller: 'mvUserListCtrl',
-            resolve:{
-                auth: function(mvIdentity, $q) {
-                    if(mvIdentity.currentUser && mvIdentity.currentUser.roles.indexOf('admin') > -1){
-                        return true;
-                    } else{
-                        return $q.reject('not authorized');
-                    }
-                }
-            }
+            resolve: routeRoleChecks.admin
         });
 
 });
 
-angular.module('app').run(function($rootScope, $location){
-    $rootScope.$on('$routeChangeError', function(evt, current, previous, rejection){
-        if(rejection === 'not authorized'){
+angular.module('app').run(function ($rootScope, $location) {
+    $rootScope.$on('$routeChangeError', function (evt, current, previous, rejection) {
+        if (rejection === 'not authorized') {
             $location.path('/');
         }
     });
 });
-
